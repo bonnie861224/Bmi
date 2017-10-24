0b288ec7e9411085d423cab5a70396788f701d21
bonnie chang <b22379222@gmail.com>
2017-10-24 11:52:17 +0800

    calculate bmi

--- app/src/main/java/bonnie/com/bmi/MainActivity.java
+++ app/src/main/java/bonnie/com/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package bonnie.com.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){
+    //System.out.println("what???"); 用android monitor可以看到這行有被執行
+    // Log.wtf()    全名what a terrible failure
+        Log.d("MainActivity","testing bmi method");  //除錯
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+     //   String s = edWeight.getText().toString();
+     //   float weight = Float.parseFloat(s); 22,23可以變24
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
+        float bmi =weight/(height*height);
+        Log.d("MainActivity","Your BMI is "+bmi);
     }
 }
