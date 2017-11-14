be84ab6a5b9c5ad04ba4f573c733ac4197b064d6
bonnie chang <b22379222@gmail.com>
2017-11-14 09:22:45 +0800

    Advice

--- app/src/main/java/bonnie/com/bmi/MainActivity.java
+++ app/src/main/java/bonnie/com/bmi/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getResources().getString(R.string.app_name);
         Button bHelp = (Button) findViewById(R.id.b_help);
-        bHelp.setOnClickListener( new View.OnClickListener() {
+        bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new AlertDialog.Builder(MainActivity.this).setTitle("xxx")
@@ -26,29 +26,40 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("OK", null)
                         .show();
             }
-    });
-};
+        });
+    }
+
+    ;
 
 
-
-
-    public void bmi(View view){
+    public void bmi(View view) {
      /*System.out.println("what???"); 用android monitor可以看到這行有被執行
          Log.wtf()    全名what a terrible failure*/
-        Log.d("MainActivity","testing bmi method");  //除錯
+        Log.d("MainActivity", "testing bmi method");  //除錯
         EditText edWeight = (EditText) findViewById(R.id.ed_weight);
         EditText edHeight = (EditText) findViewById(R.id.ed_height);
      /*String s = edWeight.getText().toString();
          float weight = Float.parseFloat(s); 22,23可以變24*/
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
-        float bmi =weight/(height*height);
-        new AlertDialog.Builder(this)
-                .setMessage(getString(R.string.your_bmi_is)+ bmi)
-                .setTitle(R.string.bmi_title)
-                .setPositiveButton(R.string.ok,null)
-                .show();
+        float bmi = weight / (height * height);
+        if (bmi < 20) {
+            new AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.your_bmi_is) + bmi + "請多吃點")
+                    .setTitle(R.string.bmi_title)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+        } else {
+
+            new AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.your_bmi_is) + bmi)
+                    .setTitle(R.string.bmi_title)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+
      /*   Log.d("MainActivity","Your BMI is "+bmi);
         Toast.makeText(this, "Your BMI is"+bmi,  Toast.LENGTH_LONG).show();*/
+        }
     }
 }
+
